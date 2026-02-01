b2bcb41c7b7b7941091a37c524dda93f6cd47d39
ekahPruthvi <p1sribhat@gmail.com>
2026-02-01 10:36:54 +0530

    welcome

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,21 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fs;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn is_wind_empty() -> bool {
+    let path = "/var/lib/cynager/wind/";
+    match fs::read_dir(path) {
+        Ok(mut entries) => entries.next().is_none(),
+        Err(_) => true, 
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            is_wind_empty
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,8 @@ use sycamore::futures::spawn_local_scoped;
 use sycamore::prelude::*;
 use sycamore::web::events::SubmitEvent;
 use wasm_bindgen::prelude::*;
+use std::fs;
+use std::io::{Read, Write};
 
 #[wasm_bindgen]
 extern "C" {
@@ -10,57 +12,34 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
-}
-
-
 #[component]
 pub fn App() -> View {
-    let name = create_signal(String::new());
-    let greet_msg = create_signal(String::new());
+    let folder_empty = create_signal(None::<bool>);
 
-    let greet = move |e: SubmitEvent| {
-        e.prevent_default();
-        spawn_local_scoped(async move {
-            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-            let args = serde_wasm_bindgen::to_value(&GreetArgs {
-				name: &name.get_clone()
-			})
-			.unwrap();
-            let new_msg = invoke("greet", args).await;
-            greet_msg.set(new_msg.as_string().unwrap());
-        })
-    };
+    spawn_local_scoped(async move {
+        let res = invoke("is_wind_empty", JsValue::NULL).await;
+        folder_empty.set(res.as_bool());
+    });
 
     view! {
         main(class="container") {
-            h1 {
-                "Welcome to Tauri + Sycamore"
-            }
+            (match folder_empty.get() {
+                None => view! { p { "Checking system..." } },
+                
+                Some(true) => view! {
+                    div(class="welcome") {
+                        "No saves found. Create new?"
+                        button { "New Save" }
+                    }
+                },
 
-            div(class="row") {
-                a(href="https://tauri.app", target="_blank") {
-                    img(src="public/tauri.svg", class="logo tauri", alt="Tauri logo")
-                }
-                a(href="https://sycamore.dev", target="_blank") {
-                    img(src="public/sycamore.svg", class="logo sycamore", alt="Sycamore logo")
-                }
-            }
-            p {
-                "Click on the Tauri and Sycamore logos to learn more."
-            }
-
-            form(class="row", on:submit=greet) {
-                input(id="greet-input", bind:value=name, placeholder="Enter a name...")
-                button(r#type="submit") {
-                    "Greet"
-                }
-            }
-            p {
-                (greet_msg)
-            }
+                Some(false) => view! {
+                    div(class="welcome") {
+                        "Saves found! Load last?"
+                        button { "Load Save" }
+                    }
+                },
+            })
         }
     }
 }
