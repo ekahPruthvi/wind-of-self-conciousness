54fc3a4fe74e1f38876270091c44be1bc1069a43
ekahPruthvi <p1sribhat@gmail.com>
2026-02-01 16:11:05 +0530

    create save file

--- src/main.js
+++ src/main.js
@@ -1,21 +1,47 @@
 const { invoke } = window.__TAURI__.core;
-import { open } from '@tauri-apps/plugin-opener';
 
+const screentxt = document.getElementById("sctxt");
+const pbtn = document.getElementById("pri_btn");
+const inp = document.getElementById("saveinp")
 
 async function greet() {
   const isEmpty = await invoke('is_wind_empty');
   console.log("Is the folder empty?", isEmpty);
   if (isEmpty) {
-    const screen = document.getElementById("screen");
-    const pbtn = document.getElementById("pri_btn");
-    screen.innerHTML="No Save FIles found";
     pbtn.innerText="Create New";
+  } else {
+    screentxt.classList.add("hiddden");
+    pbtn.innerText="Login";
+    pbtn.classList.remove("usable_btn");
+
   }
 }
 
-function primary_btn_action() {
-  
+
+
+async function create(name) {
+  try {
+    await invoke('create_save_file', { name });
+    console.log("Save created!");
+  } catch (err) {
+    console.error("Rust threw an error:", err);
+  }
 }
+
+pbtn.addEventListener("click", () => {
+  if (pbtn.textContent === "Create New") {
+    screentxt.classList.add("hiddden")
+    inp.classList.remove("hiddden");
+    pbtn.textContent = "Login";
+  }
+});
+
+inp.addEventListener("keydown", (event) =>{
+  if (event.key === "Enter") {
+    create(inp.value);
+    // login page and shi
+  }
+});
 
 
 greet()
